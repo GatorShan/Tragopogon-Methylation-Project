cb83b71a1320149883b7f39913722e3e66a304dd
GatorShan <shan158538@ufl.edu>
2019-09-30 17:40:16 -0400

    Create run_methylpy_1.0.py

---
+++ run_methylpy_1.0.py
@@ -0,0 +1,35 @@
+import multiprocessing
+import subprocess
+import shlex
+from methylpy.call_mc_pe import merge_sorted_multimap_pe, call_methylated_sites_pe # add _pe
+from methylpy.call_mc_se import remove_clonal_bam
+import os
+
+num_procs = 16
+reference_fasta = "/ufrc/soltis/shan158538/Methylation/OutPut/Build_reference/Tdub_lambda.fasta" # CHECK
+sample = "T.dubius_3060-1-4"
+path_to_output = "/ufrc/soltis/shan158538/Methylation/OutPut/methylpy_processing_data_3.1/" # CHECK
+
+# Calling methylated sites
+output_bam_file = path_to_output + sample + "_libA_processed_reads_no_clonal.bam"
+call_methylated_sites_pe(output_bam_file,
+                         sample,
+                         reference_fasta,
+                         unmethylated_control="NC_001416.1_lambda:",
+                         sig_cutoff=0.01,
+                         num_procs=num_procs,
+                         num_upstr_bases=0,
+                         num_downstr_bases=2,
+                         generate_mpileup_file=True,
+                         compress_output=True,
+                         bgzip=False,
+                         path_to_bgzip="",
+                         path_to_tabix="",
+                         min_cov=2,
+                         binom_test=True,
+                         remove_chr_prefix=True,
+                         sort_mem="500M",
+                         path_to_files=path_to_output,
+                         path_to_samtools="",
+                         min_base_quality=1,
+                         keep_temp_files=True)
