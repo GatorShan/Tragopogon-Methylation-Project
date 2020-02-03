e10dfd4837a8f00b839e91f860f864f080275f67
GatorShan <shan158538@ufl.edu>
2020-02-03 10:37:00 -0500

    Add files via upload

---
+++ bismark_analysis/bismark_methylation_extractor_S1_V1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH --job-name=bismark_methylation_extractor_S1_V1
+#SBATCH --mail-user=shan158538@ufl.edu
+#SBATCH --mail-type=FAIL,END
+#SBATCH --output=bismark_methylation_extractor_S1_V1_%j.out
+#SBATCH --error=bismark_methylation_extractor_S1_V1_%j.error
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=10gb
+#SBATCH --time=4-00:00:00
+#SBATCH --qos=soltis-b
+
+date;hostname;pwd
+
+IN=/orange/soltis/shan158538/Methylation_output/bismark_deduplicate
+OUT=/orange/soltis/shan158538/Methylation_output/bismark_methylation_extractor/S1
+
+module purge
+module load bismark/0.22.3
+
+bismark_methylation_extractor \
+	${IN}/S1_cat_R1_val_1_bismark_bt2_pe_nameSorted.deduplicated.bam \
+	--paired-end \
+	--no_overlap \
+	--output ${OUT} \
+	--gzip \
+	--multicore 3 \
+	--bedGraph \
+	--scaffolds
+
+date
