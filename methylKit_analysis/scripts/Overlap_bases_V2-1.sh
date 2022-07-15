e1c5b9979c8dea7df8ad2651d2e119e636d7c3be
Shengchen Shan <shan158538@ufl.edu>
2022-07-15 16:35:57 -0400

    Add files via upload

---
+++ methylKit_analysis/scripts/Overlap_bases_V2-1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=Overlap_bases_V2-1
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=shan158538@ufl.edu	
+#SBATCH --ntasks=1
+#SBATCH --mem=50gb
+#SBATCH --time=00:20:00
+#SBATCH --output=Overlap_bases_V2-1_%j.out 
+#SBATCH --error=Overlap_bases_V2-1_%j.error
+#SBATCH --qos=soltis-b
+
+date; hostname; pwd
+
+cd /blue/soltis/shan158538/Methylation/OutPut/Overlap_bases/CpG_mincov_3
+
+module purge
+module load python
+
+Overlap_bases.V2.py Tdu_1_cov3_CpG.txt Tdu_2_cov3_CpG.txt Tms_1_du_cov3_CpG.txt Tms_1_pr_cov3_CpG.txt Tms_2_du_cov3_CpG.txt Tms_2_pr_cov3_CpG.txt Tpr_1_cov3_CpG.txt Tpr_2_cov3_CpG.txt
+
+date
